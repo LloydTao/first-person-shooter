88d41a24769cf45c1a4df532cf7ba44e7693a5d0
LloydTao <LL23@live.co.uk>
2020-06-07 21:20:10 +0100

    Make main an entry point to client/server mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+#include "client.h"
+#include "server.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Client [0] or server [1]: ";
+
+    int input;
+    std::cin >> input;
+
+    if (input == 0) {
+        client_main();
+    } else {
+        server_main();
+    }
+
+    std::cout << std::endl << "Exit: " << std::endl;
+    std::cin >> input;
     return 0;
 }
